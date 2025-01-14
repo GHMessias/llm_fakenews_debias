71a97a6202066516e3767118b646d57e1c6b10a2
ghmessias <ghmessias@estudante.ufscar.br>
2025-01-14 13:41:51 -0300

    initial tests

---
+++ ollama_news_debias.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from ollama import chat
+from ollama import ChatResponse
+import utils.utils as utils
+from tqdm import tqdm
+
+tqdm.pandas()
+
+args = utils.parse_arguments()
+if args.config:
+        config_params = utils.load_config_from_json(args.config)
+        # Atualiza os parâmetros do argparse com os valores do JSON
+        for key, value in config_params.items():
+            setattr(args, key, value)
+
+df = pd.read_csv(args.input_data_path, sep = '\t')
+
+df = df[df['label'] == 1]
+if args.number_of_samples:
+     df = df.sample(n = args.number_of_samples)
+     
+
+def query_olmo2(news):
+    prompt = args.input_instruction + news
+
+    response: ChatResponse = chat(model=args.llm_model, messages=[
+        {
+        'role': 'user',
+        'content': prompt
+        }
+    ])
+
+    return response.message.content.replace('\n', ' ')
+
+# df['Model Answer'] = df['news'].apply(query_olmo2)
+df['Model Answer'] = df.progress_apply(lambda row: query_olmo2(row['news']) if row['label'] == 1 else None, axis=1)
+
+
+df.to_csv(args.output_path, sep = '\t')
---
+++ utils/utils.py
@@ -0,0 +1,24 @@
+import argparse
+import json
+
+def parse_arguments():
+    '''
+    Function to collect the arguments
+    '''
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, help='Path to the JSON config file with parameters')
+    parser.add_argument('--input_data_path', type = str, help = 'Path to dataset in csv format')
+    parser.add_argument('--input_column', type = str, help = 'csv Column that the model will use to debias')
+    parser.add_argument('--input_instruction', type = str, help = 'instruction of the model to debias', default = 'Suponha que você está trabalhando para identificar notícias falsas a partir de um site de checagem. Seu objetivo é, dado texto do site de checagem, gerar a possível notícia falsa que foi divulgada. Seja direto, respondendo somente a notícia falsa em questão, sem justificativas ou textos excedentes. Sabendo disso, identifique a notícia falsa que foi divulgada a partir do seguinte texto do site de checagem: ')
+    parser.add_argument('--output_path', type = str, help = 'output path and name', default = 'output.csv')
+    parser.add_argument('--number_of_samples', type = int, help = 'number of samples to run in test cases')
+    parser.add_argument('--llm_model', type = str, default = 'llama3.1', help = 'OllaMa model name')
+
+    return parser.parse_args()
+
+def load_config_from_json(json_file):
+    '''Function to load parameters from a JSON file'''
+    with open(json_file, 'r') as f:
+        config = json.load(f)
+    return config
