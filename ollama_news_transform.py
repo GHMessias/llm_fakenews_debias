427cf7d87b6414fd3dbc513623dd578ac61098b3
ghmessias <ghmessias@estudante.ufscar.br>
2025-01-14 09:43:18 -0300

    main files

--- ollama_news_transform.py
+++ ollama_news_transform.py
@@ -4,7 +4,8 @@ from ollama import ChatResponse
 
 df = pd.read_csv('datasets/FactCheckedNews/Dataset/Fact_checked_news.tsv', sep = '\t', header = None)
 df.columns = ['path', 'news', 'label']
-df = df.head()
+df = df[df["label"] == 1].head()
+print(df)
 
 def query_olmo2(news):
     prompt = 'Suponha que você está trabalhando para identificar notícias falsas a partir de um site de checagem. Seu objetivo é, dado texto do site de checagem, gerar a possível notícia falsa que foi divulgada. Seja direto, respondendo somente a notícia falsa em questão, sem justificativas ou textos excedentes. Sabendo disso, identifique a notícia falsa que foi divulgada a partir do seguinte texto do site de checagem: ' + news
